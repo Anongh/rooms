89ce126f4433a30a2e2318fb63a693a30c7618f2
Bryce Matsumori <bmats@users.noreply.github.com>
2017-11-29 15:31:04 -0800

    working levels

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+    [SerializeField] private Vector3 _speed;
+
+    private void Update() {
+        transform.Rotate(_speed * Time.deltaTime);
+    }
+}
